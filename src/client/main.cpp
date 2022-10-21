0f3b1b40ee36055309c43529e7fa9fbc31b734b8
jathurchan <admin@jathurchan.com>
2022-10-21 21:05:27 +0200

    Simple Test

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,26 +1,14 @@
-/*#include <iostream>
-
-// Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
-#include <SFML/Graphics.hpp>
-
-void testSFML() {
-    sf::Texture texture;
-}
-
-// Fin test SFML
-
-#include <state.h>
+#include <iostream>
+#include <hello_world.hpp>
 
 using namespace std;
-using namespace state;
+
+void World::Hello () {
+    std::cout << "Hello, world!\n";
+}
 
 int main(int argc,char* argv[])
 {
-    Exemple exemple;
-    exemple.setX(53);
-
-    cout << "It works !" << endl;
-
+    World::Hello();
     return 0;
 }
-*/
