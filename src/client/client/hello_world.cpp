381fc33b21fc3fc3f1cf697d62b2bae02c4066c6
jathurchan <admin@jathurchan.com>
2022-10-21 21:51:02 +0200

    Fixed bugs

--- src/client/client/hello_world.cpp
+++
@@ -1,8 +0,0 @@
-#include "client.hpp"
-#include <iostream>
-
-namespace client {
-    client::helloWorld() {
-        std::cout << "Hello, world!\n";
-    }
-}
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <client.hpp>
 
+namespace client {
+    client::helloWorld() {
+        std::cout << "Hello, world!\n";
+    }
+}
+
 using namespace client;
 
 int main(int argc,char* argv[])
