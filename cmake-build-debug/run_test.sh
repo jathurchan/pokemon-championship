f745957186584fbacfb8ad17c61c281972268f90
akossyfidis <alexkossyfidis@outlook.fr>
2022-10-28 20:20:11 +0200

    Fixed build + added profiler

---
+++ cmake-build-debug/run_test.sh
@@ -0,0 +1,2 @@
+cd "/home/alexandros/Documents/ENSEA/ENSEA_3A/plt/cmake-build-debug/test"
+ctest --timeout 300 ||/bin/true
---
+++ cmake-build-debug/test/shared/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/alexandros/Documents/ENSEA/ENSEA_3A/plt/test/shared
+# Build directory: /home/alexandros/Documents/ENSEA/ENSEA_3A/plt/cmake-build-debug/test/shared
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_shared_dummy "/home/alexandros/Documents/ENSEA/ENSEA_3A/plt/cmake-build-debug/test/shared/test_shared_dummy")
+set_tests_properties(test_shared_dummy PROPERTIES  WORKING_DIRECTORY "/home/alexandros/Documents/ENSEA/ENSEA_3A/plt" _BACKTRACE_TRIPLES "/home/alexandros/Documents/ENSEA/ENSEA_3A/plt/test/CMakeLists.txt;77;add_test;/home/alexandros/Documents/ENSEA/ENSEA_3A/plt/test/shared/CMakeLists.txt;2;add_custom_test;/home/alexandros/Documents/ENSEA/ENSEA_3A/plt/test/shared/CMakeLists.txt;0;")
