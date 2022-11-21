ef6cdcc7320df879e93a5b0bd2b39a262e386efe
jathurchan <admin@jathurchan.com>
2022-11-21 17:35:05 +0100

    Reboot

--- src/shared/state/ChampionshipStatus.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by alexandros on 30/10/22.
-//
-
-#include "ChampionshipStatus.hpp"
-
-namespace state {
-
-}
--- src/shared/state/Type.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by alexandros on 30/10/22.
-//
-#include "Type.hpp"
-
-namespace state {
-
-}
