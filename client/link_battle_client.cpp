3eb4fa4a42e9d46ffc4d7ea794e1427102f6283a
jathurchan <admin@jathurchan.com>
2022-12-10 00:03:11 +0100

    Client-server prototypes

--- client/link_battle_client.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by JS on 22/11/2022.
-//
-
-#include "link_battle_client.h"
-
-namespace client {
-
-}   // namespace client
--- server/link_battle_server.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by JS on 22/11/2022.
-//
-
-#include "link_battle_server.h"
-
-namespace server {
-
-}   // namespace server
