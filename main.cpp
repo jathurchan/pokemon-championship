49147b122e170b5dddaaf6bc190bdf453962b5b8
jathurchan <admin@jathurchan.com>
2022-12-14 18:41:54 +0100

    Added tests for LinkCode

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "LinkCode.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    // Tests for LinkCode
+    LinkCode firstLC = LinkCode("0123");
+    LinkCode secondLC = LinkCode("123123");
+    LinkCode thirdLC = LinkCode("123d");
+
+    if (firstLC.Valid()) {
+        std::cout << firstLC.GetContent() << std::endl;
+    }
+
+    if (secondLC.Valid()) {
+        std::cout << secondLC.GetContent() << std::endl;
+    }
+
+    if (thirdLC.Valid()) {
+        std::cout << thirdLC.GetContent() << std::endl;
+    }
     return 0;
 }
